c50716f2d3d4bc958804ee5e25270f2e84b07537
KatyIftimie <ecaterina.iftimie@gmail.com>
2019-04-10 15:50:44 +0100

    day one

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'viorel', age: 77 },
+      { name: 'dancila', age: 99999 },
+      { name: 'vasilica', age: 79 }
+    ]
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>tytyty</h1>
+        <button>Switch name</button>
+        <Person name ={this.state.persons[0].name} age =  {this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age =  {this.state.persons[1].age}>proasta </Person>
+        <Person name ={this.state.persons[2].name} age =  {this.state.persons[2].age}/>
       </div>
     );
   }
