a80c0d7dff03ed3e658e3b6956a498b2250a92f1
KatyIftimie <ecaterina.iftimie@gmail.com>
2019-04-12 13:29:29 +0100

    day 2 . manipulating data with react and react hooks

--- src/App.js
+++ src/App.js
@@ -11,18 +11,81 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DO NOT DO THIS :this.state.persons[0].name= "maximilian";
+    this.setState({
+      persons: [
+        { name: newName, age: 77 },
+        { name: 'dancila', age: 99999 },
+        { name: 'vasilica', age: 799 }
+      ] 
+    })
+  }
 
-  render() {
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'viorel', age: 77 },
+        { name: event.target.value, age: 99999 },
+        { name: event.target.value, age: 799 }
+      ] 
+    })
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid red',
+      padding: '8px',
+      cursor: 'pointer' 
+    };
     return (
       <div className="App">
         <h1>tytyty</h1>
-        <button>Switch name</button>
-        <Person name ={this.state.persons[0].name} age =  {this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age =  {this.state.persons[1].age}>proasta </Person>
-        <Person name ={this.state.persons[2].name} age =  {this.state.persons[2].age}/>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('valentin')}>Switch name</button> 
+        <Person 
+          name ={this.state.persons[0].name} 
+          age =  {this.state.persons[0].age}/>
+        <Person 
+          click={this.switchNameHandler.bind(this, 'cartof')}
+          name={this.state.persons[1].name} 
+          changed={this.nameChangedHandler}
+          age =  {this.state.persons[1].age}>proasta </Person>
+        <Person 
+          changed={this.nameChangedHandler}
+          name ={this.state.persons[2].name} 
+          age =  {this.state.persons[2].age}/>
       </div>
     );
   }
 }
 
+
 export default App;
+
+
+// const app = props => {
+  //   const [ personsState, setPersonsState] = useState({
+  //     persons: [
+  //       { name: 'viorel', age: 77 },
+  //       { name: 'dancila', age: 99999 },
+  //       { name: 'vasilica', age: 79 }
+  //     ],
+  //     otherState: 'some value'
+  //   });
+  
+  //   const switchNameHandler = () => {
+  //     // console.log('Was clicked!');
+  //     // DO NOT DO THIS :this.state.persons[0].name= "maximilian";
+  //     setPersonsState({
+  //       persons: [
+  //         { name: 'viorelrere', age: 77 },
+  //         { name: 'dancila', age: 99999 },
+  //         { name: 'vasilica', age: 799 }
+  //       ] 
+  //     });
+  //   };
